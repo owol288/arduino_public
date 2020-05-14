07af04a64236e6ee1aebb19d94a23701554106a9
Sergio Lucio <45217959+srg-lc@users.noreply.github.com>
2020-05-14 11:09:50 -0400

    inicio de integração sd card

--- fingerprint_sketch/fingerprint_display/fingerprint_display/fingerprint_display.ino
+++ fingerprint_sketch/fingerprint_display/fingerprint_display/fingerprint_display.ino
@@ -14,6 +14,7 @@ LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
 int id,contador,param,retorno;
 String mensagem;
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
 void setup() {
   //pinos led
   pinMode(ledVermelho,OUTPUT);
